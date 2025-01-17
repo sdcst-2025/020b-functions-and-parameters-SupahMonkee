cba07d3e8eae52ab664c13d33f33971335838eb0
Lucas Detlor <lucasjd0112@gmail.com>
2025-01-17 11:34:16 -0800

    v3

--- problem2.py
+++ problem2.py
@@ -9,8 +9,11 @@ Note that the coordinates should be signed (positive or negative) floats
 """
 import math
 
-def distance():
-    return
+def distance(t1, t2):
+    distx = t1[0] - t2[0]
+    disty = t1[1] - t2[1]
+    hyp = math.sqrt(math.pow(distx, 2) + math.pow(disty, 2))
+    return hyp
 
 if __name__ == "__main__":
     d = distance( (2,4) , (6,3) )
@@ -18,4 +21,4 @@ if __name__ == "__main__":
     d = distance( (-3,2.2) , (1,2))
     assert round(d,3) == 4.005
 
-
+#done!
